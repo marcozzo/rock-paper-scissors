0ebcc76d8adfa75d7293fa0764ad6ebf1576f221
marcozzo <130609450+marcozzo@users.noreply.github.com>
2024-03-12 11:24:34 -0300

    Implemented function for one round
    playRound() function compares playerChoice
    and computerChoice paremeters, then returns a
    victory/defeat message according to the result.

--- jankenpon.js
+++ jankenpon.js
@@ -2,13 +2,51 @@ console.log("Hey! Welcome to this console-based Rock, Paper Scissors game!")
 
 function getComputerChoice () {
   let randomN = Math.random() * 100;
-  console.log(randomN);
 
   if (randomN <= 33) {
-    return "Rock"
+    return "rock"
   } else if (randomN <= 66) {
-    return "Paper"
+    return "paper"
   } else {
-    return "Scissors"
+    return "scissors"
+  }
+}
+
+let playRound = function (playerChoice, computerChoice) {
+
+  playerChoice = playerChoice.toLowerCase()
+
+  if (playerChoice === computerChoice) {
+    return `It's a tie! Both chose ${playerChoice}`
+  }
+
+  switch (playerChoice) {
+
+    case 'rock':
+      if (computerChoice === 'scissors') {
+        return "You Won! Rock beats Scissors"
+      } else {
+      return "You Lose! Paper beats Rock"
+      }
+      break
+
+    case 'paper':
+      if (computerChoice === 'rock') {
+        return "You Won! Paper beats Rock"
+      } else {
+        return "You Lose! Scissors beats Paper"
+      }
+      break
+
+    case 'scissors':
+      if (computerChoice === 'paper') {
+        return "You Won! Scissors beats Paper"
+      } else {
+        return "You Lose! Rock beats Scissors"
+      }
+
+    default:
+    return `Ummm... That one is not valid.
+Try paper, rock or scissors.`;
   }
 }

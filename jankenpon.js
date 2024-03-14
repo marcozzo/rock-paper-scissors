242319160273dec384632a3e496d77e71c690c7e
marcozzo <130609450+marcozzo@users.noreply.github.com>
2024-03-14 20:06:12 -0300

    Implemented playGame() and fullMatch()
    playGame() function:
    
        #1
        - asks for user's and generates the computer's
        move;
    
        #2
        - runs playRound() passing those moves as
        arguments;
        - adds points to the corresponding opponent;
        - console.logs a message stating the outcome;
    
        #3
        - then finally shows opponents' score and
        number of scored rounds.
    
    fullMatch() function:
    
        - play a full game until number or non-tied
        rounds is 5;
    
        - print match result message;
    
        - reset stats.

--- jankenpon.js
+++ jankenpon.js
@@ -1,8 +1,15 @@
-console.log("Hey! Welcome to this console-based Rock, Paper Scissors game!")
+console.log("Hey! Welcome to this console-based Rock, Paper Scissors game.\n\n" +
+"Let's Begin! To play, enter 'fullMatch()'")
 
+
+let player = 0;
+let comp = 0;
+
+//GENERATE RANDOM NUMBER FOR COMPUTER
 function getComputerChoice () {
   let randomN = Math.random() * 100;
 
+//RETURN MOVE DEPENDING ON NUMBER RANGE
   if (randomN <= 33) {
     return "rock"
   } else if (randomN <= 66) {
@@ -12,41 +19,105 @@ function getComputerChoice () {
   }
 }
 
-let playRound = function (playerChoice, computerChoice) {
+function playRound(playerChoice, computerChoice) {
 
-  playerChoice = playerChoice.toLowerCase()
+//IF NOT A A VALID MOVE...
+  if (typeof(playerChoice) !== 'string' &&
+  !(  playerChoice  === 'rock'
+  ||  playerChoice  === 'paper'
+  ||  playerChoice  === 'scissors') ) {
+    return "Ummm... That doesn't make sense.\n"
+        +  "          Try again!";
+  }
+//IF A VALID MOVE
+  else {
+        playerChoice = playerChoice.toLowerCase()
+        console.log("\n      " + (playerChoice + " X " + computerChoice).toUpperCase() + "\n\n")
 
-  if (playerChoice === computerChoice) {
-    return `It's a tie! Both chose ${playerChoice}`
+//-----IF TIE
+    if (playerChoice === computerChoice) {
+      return `It's a tie! Both chose ${playerChoice}`;
+    }
+//-----IF NOT TIE, EVALUATE FOR ALL POSSIBLE CASES
+    else {
+
+      switch (playerChoice) {
+
+        case 'rock':
+          if (computerChoice === 'scissors') {
+            return "You Won! Rock beats Scissors"
+          } else {
+          return "You Lose! Paper beats Rock"
+          }
+          break
+
+        case 'paper':
+          if (computerChoice === 'rock') {
+            return "You Won! Paper beats Rock"
+          } else {
+            return "You Lose! Scissors beats Paper"
+          }
+          break
+
+        case 'scissors':
+          if (computerChoice === 'paper') {
+            return "You Won! Scissors beats Paper"
+          } else {
+            return "You Lose! Rock beats Scissors"
+          }
+          break
+
+        default:
+        return "Ummm... That doesn't make sense.\n"
+              +  "          Try again!";
+      }
+
+
+    }
+
   }
 
-  switch (playerChoice) {
 
-    case 'rock':
-      if (computerChoice === 'scissors') {
-        return "You Won! Rock beats Scissors"
-      } else {
-      return "You Lose! Paper beats Rock"
-      }
-      break
+}
 
-    case 'paper':
-      if (computerChoice === 'rock') {
-        return "You Won! Paper beats Rock"
-      } else {
-        return "You Lose! Scissors beats Paper"
-      }
-      break
+function playGame() {
 
-    case 'scissors':
-      if (computerChoice === 'paper') {
-        return "You Won! Scissors beats Paper"
-      } else {
-        return "You Lose! Rock beats Scissors"
-      }
+////ASK PLAYER'S & COMPUTER'S CHOICE
+    let playerChoice = prompt('Rock, Paper or Scissors?');
+    const result = playRound(playerChoice, getComputerChoice());
 
-    default:
-    return `Ummm... That one is not valid.
-Try paper, rock or scissors.`;
+////TEST FOR WIN, LOSE OR WRONG
+    if ( result.includes('You Won!') ) {
+      ++player;
+      console.log(result)
+    }
+    else if ( result.includes('You Lose!') ) {
+      ++comp;
+      console.log(result)
+    }
+    else {
+      console.log(result)
+    }
+////
+    console.log('PLAYER:', player, ' COMP:', comp)
+    console.log('ROUNDS: ' + (player + comp));
+}
+
+function fullMatch (){
+
+//PLAY FOR 5 SCORED ROUNDS
+  while( player + comp < 5){
+    playGame()
   }
+
+  if (player > comp) {
+    return "Congratulations, you won! Let's play another one?";
+  }
+  else {
+    return "Too bad you lost... Let's play another one?"
+  }
+
+  player = 0;
+  comp = 0;
+
 }

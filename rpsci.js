149f5f2857a09908b50eeafc4d77105794b534bf
marcozzodaro <130609450+marcozzo@users.noreply.github.com>
2025-01-08 19:24:47 -0300

    MAKE RESPONSIVE TO HTML USER INTERACTION
    - diplay all console messages
    on HTML page instead.

--- rpsci.js
+++ rpsci.js
@@ -1,13 +1,12 @@
-let humanScore = 0;
+let playerScore = 0;
 let computerScore = 0;
-//let roundCount = 1;
 
 const buttons = document.querySelector('#player-selection');
 const rock = document.querySelector('#rock')
 const paper = document.querySelector('#paper')
 const scissors = document.querySelector('#scissors')
 
-const result = document.querySelector('#results');
+const result = document.querySelector('#round');
 
 let playerChoice = '';
 
@@ -25,30 +24,35 @@ function playRound(event) {
 
     //CASES
 
-    console.log('player' + playerChoice);
-    console.log('computer' + computerChoice);
+    console.log('player: ' + playerChoice);
+    console.log('computer: ' + computerChoice);
 
     checkResult(playerChoice, computerChoice)
 
 }
+
+/*
+if(playerScore === 5 || computerScore === 5) {
+
+}*/
 
 
 ////RESET ROUNDS
 //  roundCount = 1;
 
 /*////FINAL MESSAGE////
-if (humanScore === computerScore) {
-    humanScore = 0;
+if (playerScore === computerScore) {
+    playerScore = 0;
     computerScore = 0;
     return "The game is a tie!"
 }
-else if (humanScore > computerScore) {
-    humanScore = 0;
+else if (playerScore > computerScore) {
+    playerScore = 0;
     computerScore = 0;
     return "Congratulations! You won the game."
 }
 else {
-    humanScore = 0;
+    playerScore = 0;
     computerScore = 0;
     return "You lost the game."
 }*/
@@ -78,55 +82,47 @@ let getComputerChoice = function () {
 //COMPARE CHOICES AND DISPLAY RESULT AND SCORE
 function checkResult(human, computer) {
 
-    const HUMAN = "color: cyan"
-    const COMPUTER = "color: red"
-
     if (human === computer) {
+        result.style.fontSize = '25px';
         result.textContent = "Tie!";
     }
 
 
     //PLAYER IS ROCK
     else if (human + computer === "rockscissors") {
-        result = "win"
-        console.log("ROCK beats SCISSORS! \nYou get a point!")
+        result.textContent = "ROCK beats SCISSORS! \nYou get a point!";
+        result.style.fontSize = '15px';
+        playerScore ++
     }
     else if (human + computer === "rockpaper") {
-        result = "lose"
-        console.log("PAPER beats ROCK! \nComputer gets a point.")
+        result.textContent = "PAPER beats ROCK! \nComputer gets a point."
+        result.style.fontSize = '15px';
+        playerScore ++
     }
 
     //PLAYER IS PAPER
     else if (human + computer === "paperrock") {
-        result = "win"
-        console.log("PAPER beats ROCK! \nYou get a point!")
+        result.textContent = "PAPER beats ROCK! \nYou get a point!"
+        result.style.fontSize = '15px';
+        playerScore ++
     }
     else if (human + computer === "paperscissors") {
-        result = "lose"
-        console.log("SCISSORS beats PAPER! \nComputer gets a point.")
+        result.textContent = "SCISSORS beats PAPER! \nComputer gets a point."
+        result.style.fontSize = '15px';
+        playerScore ++
     }
 
     //PLAYER IS SCISSORS
     else if (human + computer === "scissorspaper") {
-        result = "win"
-        console.log("SCISSORS beats PAPER! \nYou get a point!")
+        result.textContent = "SCISSORS beats PAPER! \nYou get a point!"
+        result.style.fontSize = '15px';
+        playerScore ++
     }
     else if (human + computer === "scissorsrock") {
-        result = "lose"
-        console.log("ROCK beats SCISSORS! \nComputer gets a point.")
+        result.textContent = "ROCK beats SCISSORS! \nComputer gets a point."
+        result.style.fontSize = '15px';
+        playerScore ++
     }
-    ++roundCount
-
-
-    /////TEST FOR VICTORY/DEFEAT
-    if (result === "win") {
-        ++humanScore;
-    }
-    else if (result === "lose") {
-        ++computerScore;
-    }
-
-    console.log("%cPLAYER: " + humanScore, HUMAN);
-    console.log("%cCOMPUTER: " + computerScore, COMPUTER);
+    //++roundCount (no more roundCount variable)
 
 }
